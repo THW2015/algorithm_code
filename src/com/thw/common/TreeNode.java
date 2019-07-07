84fb91edcf81452d7619157385dfe9f3c9e28dd9
THW2015 <593659637@qq.com>
2019-07-07 17:20:57 +0800

    添加树形节点一个公共类

---
+++ src/com/thw/common/TreeNode.java
@@ -0,0 +1,13 @@
+package com.thw.common;
+
+/**
+ * 树形结构
+ * @author 谭皓文
+ * @date 2019/7/7 17:16
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
