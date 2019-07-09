9c6d97bdfbc01f9b7ff727dc91603de45ab1e5f3
THW2015 <593659637@qq.com>
2019-07-09 22:27:05 +0800

    链表节点

---
+++ src/com/thw/common/Node.java
@@ -0,0 +1,14 @@
+package com.thw.common;
+
+/**
+ * 链表节点
+ * @author 谭皓文
+ * @date 2019/7/9 22:25
+ */
+public class Node {
+    public int value;
+    public Node next;
+    public Node (int value) {
+        this.value = value;
+    }
+}
