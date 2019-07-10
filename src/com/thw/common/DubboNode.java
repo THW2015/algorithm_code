5b1a32b3f7d9d474abc087b3a55f9a8107c03918
THW2015 <593659637@qq.com>
2019-07-10 21:40:01 +0800

    双链表节点

---
+++ src/com/thw/common/DubboNode.java
@@ -0,0 +1,17 @@
+package com.thw.common;
+
+/**
+ * 双链表节点
+ * @author 谭皓文
+ * @date 2019/7/10 21:38
+ */
+public class DubboNode {
+
+    public int value;
+    public Node next;
+    public Node last;
+    public DubboNode (int value) {
+        this.value = value;
+    }
+
+}
