33fd2f7c3e503a187ddb31c66352e076273c4e61
THW2015 <593659637@qq.com>
2019-07-12 00:14:32 +0800

    新增删除双向链表倒数第K个

--- src/com/thw/two/RemoveLastKthNode.java
+++ src/com/thw/two/RemoveLastKthNode.java
@@ -1,6 +1,8 @@
 package com.thw.two;
 
+import com.thw.common.DoubleNode;
 import com.thw.common.Node;
+import com.thw.utils.DoubleLinkedBuilderUtil;
 import com.thw.utils.LinkedBuilderUtil;
 import com.thw.utils.PrintLinkedUtil;
 
@@ -38,8 +40,44 @@ public class RemoveLastKthNode {
          return head;
      }
 
+    public static DoubleNode doRemoveDoubleLinkedLastKthNode (DoubleNode head, int lastKth) {
+        if (head == null || lastKth < 1) {
+            return head;
+        }
+        DoubleNode cur = head;
+        while (cur != null) {
+            --lastKth;
+            cur = cur.next;
+        }
+        if (lastKth == 0) {
+            head = head.next;
+            head.last = null;
+            return head;
+        }
+
+        if (lastKth > 0) {
+            return head;
+        }
+
+        cur = head;
+        lastKth += 1;
+        while (lastKth != 0) {
+            cur = cur.next;
+            lastKth++;
+        }
+        DoubleNode node = cur.next;
+        cur.next = cur.next.next;
+        if (cur.next == null) {
+            return head;
+        }
+        cur = cur.next.last;
+        node = null;
+        return head;
+    }
+
 
     public static void main(String[] args) {
-        PrintLinkedUtil.printLinked(RemoveLastKthNode.doRemoveLastKthNode(LinkedBuilderUtil.buildLinked(new int[]{1, 3, 4, 6}), 2));
+        PrintLinkedUtil.printLinked(RemoveLastKthNode.doRemoveLastKthNode(LinkedBuilderUtil.buildLinked(new int[]{1, 3, 4, 6}), 1));
+        PrintLinkedUtil.printDoubleLinked(RemoveLastKthNode.doRemoveDoubleLinkedLastKthNode(DoubleLinkedBuilderUtil.buildDoubleLinked(new int[]{1, 3, 4, 6}), 1));
     }
 }
